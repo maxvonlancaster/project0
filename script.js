3730759d6772c72fdf211c17eb5eed091aa3947f
maxvonlancaster <windchange7@gmail.com>
2023-09-25 16:39:39 +0300

    feature: jquery added

--- script.js
+++ script.js
@@ -6,25 +6,25 @@ const classNames = {
 }
 
 let i = 0;
-let list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+let list = $("#todo-list");
+const itemCountSpan = $("#item-count")
+const uncheckedCountSpan = $("#unchecked-count");
 
 function newTodo() {
-  let newToDoElement = document.createElement("div");
-  newToDoElement.id = "todo-"+i;
-  let delButton = document.createElement("button");
-  delButton.id = i;
-  delButton.textContent = "delete me"
-  delButton.addEventListener("click", deleteToDo.bind(this, i) );
-  newToDoElement.textContent = "new todo";
-  newToDoElement.appendChild(delButton);
+  let newToDoElement = $("<div></div>");
+  newToDoElement.attr("id", "todo-"+i);
+  let delButton = $("<button></button>");
+  delButton.attr("id", i);
+  delButton.text("delete me");
+  delButton.click(deleteToDo.bind(this, i))
+  newToDoElement.text("new todo");
+  newToDoElement.append(delButton);
 
-  list.appendChild(newToDoElement);
+  list.append(newToDoElement);
   i++;
 }
 
 function deleteToDo(j){
-  let toDo = document.getElementById("todo-"+j);
-  list.removeChild(toDo);
+  let toDo = $("#todo-"+j);
+  toDo.remove();
 }
